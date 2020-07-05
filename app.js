abc0281bd8c0f51d41716d1bb82d0869d86553ad
Stevenk323 <50382468+Stevenk323@users.noreply.github.com>
2020-07-05 18:41:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -11,46 +11,151 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
     //accepting form data
 app.use(express.urlencoded({ extended: true}));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+//hard coded array to simulate a DB for login purposes
+const Person_array = [{Username:'Mario123', Password:'Houston16'}, {Username: 'Steven123', Password: 'Austin123'}, {Username: 'RuthE123', Password: 'Dallas123'}];
 
 //get links
     //index
-/*
+
 app.get('/', (req,res) => {
-    res.render('index', { page: 'Home'});
+    res.render('index', { page: 'Home', loggedin: req.session.loggedin, User: req.session.Username});
 });
-*/
+
     //quote
 app.get('/quote', (req,res) => {
-    res.render('quote', { page: 'Get Quote' });
+    res.render('quote', { page: 'Get Quote', loggedin: req.session.loggedin, User: req.session.Username });
 });
 
-/*
+
     //login
 app.get('/login', (req,res) => {
-    res.render('login', { page: 'Login' });
+    res.render('login', { page: 'Login', loggedin: req.session.loggedin, User: req.session.Username});
 });
-
     //register
 app.get('/register', (req,res) => {
-    res.render('register', { page: 'Sign Up' });
+    res.render('register', { page: 'Sign Up' , loggedin: req.session.loggedin, User: req.session.Username });
 });
-    //register
-app.get('/forgotpassword', (req,res) => {
-    res.render('forgotpassword', { page: 'Forgot Password' });
+
+app.get('/logout', (req,res)=>{
+    req.session.destroy();
+    res.redirect('/');
 });
-*/
-/*
+
+
+
     //profile
 app.get('/profile', (req,res) => {
-    res.render('profile', { page: 'Profile' });
+    res.render('profile', { page: 'Profile', loggedin: req.session.loggedin, User: req.session.Username });
+});
+/*
+app.get('/get-state', (req, res) => {
+    console.log(req.body.profile);
+    //console.log(req.body);
+    //res.redirect('/profile');
 });
 */
     //quote history
 app.get('/quote_history', (req,res) => {
-    res.render('quote_history', { page: 'Quote History' });
+    res.render('quote_history', { page: 'Quote History', loggedin: req.session.loggedin, User: req.session.Username });
+});
+app.post('/add-profile', (req,res) => {
+    /*Full Name (50 characters, required)
+	- Address 1 (100 characters, required)
+	- Address 2 (100 characters, optional)
+	- City (100 characters, required)
+	- State (Drop Down, selection required) DB will store 2 character state code
+    - Zipcode (9 characters, at least 5 character code required) min is 5, max is 9 */
+    
+    /*if(!req.body.fullname){
+        return res.status(401).send({ "message": "A `full name` is required" });
+    }
+    else if(!req.body.address1){
+        return res.status(401).send({ "message": "An `address` is required" });
+    }
+    else if(!req.body.city){
+        return res.status(401).send({ "message": "A `city` is required" });
+    }
+    else if(!req.body.state){
+        return res.status(401).send({ "message": "A `state` is required" });
+    }
+    else if(!req.body.zipcode){
+        return res.status(401).send({ "message": "A `zip code` is required" });
+    }*/
+
+    //const profile_info = [{fullname: 'Ruth Soto', address1: '123 Smith St.', address2: '456 Main St.', city: 'Houston', state: 'TX', zipcode: 77123}]
+    //const profile_info = [{fullname: 'Mario Villareal', address1: '123 Smith St.', address2: '', city: 'Houston', state: 'TX', zip: '77123'}, {fullname: 'Steven Khong', address1: '456 Main St.', address2: '', city: 'Dallas', state: 'TX', zip: '74783'}, {fullname: 'Ruth Soto', address1: '783 Sam Rd.', address2: '', city: 'Austin', state: 'TX', zip: '72893'}]
+
+    const num = /\d/;
+    var words = /^[a-zA-Z ]*$/;
+    var word = /[A-z]+/;
+    const numAndLetter = /'0-9a-zA-Z'/;
+
+    var full = req.body.fullname;
+    var add1 = req.body.address1;
+    var add2 = req.body.address2;
+    var city1 = req.body.city;
+    var state1 = req.body.state;
+    var zip = req.body.zipcode;
+
+
+    if (words.test(full) && full.length <= 50) {
+        //console.log("hello");
+        if (isNaN(add1) && add1.length <= 100) {
+            //console.log("2hello");
+            if ((isNaN(add2) && add2.length <= 100) || add2.length == 0){
+                //console.log("3hello");
+                if(words.test(city1) && city1.length <= 100){
+                    //console.log("4hello");
+                    if (word.test(state1)) {//get-state
+                        if (num.test(zip) && 5 <= zip.length && zip.length <= 9) {
+                            //console.log("6hello");
+                            //res.send(profile_info);
+                            res.redirect('/');
+                        }
+                        else {
+                            //return res.status(401).send({ "message": "A `zip code` is required" });
+                            res.redirect('/profile');
+                        }
+                    }
+                    else {
+                        //return res.status(401).send({ "message": "A `state` is required" });
+                        res.redirect('/profile');
+                    }
+                }
+                else {
+                    //return res.status(401).send({ "message": "A `city` is required" });
+                    res.redirect('/profile');
+                }
+            }
+            else {
+                res.redirect('/profile');
+            }
+        }
+        else {
+            //return res.status(401).send({ "message": "An `address` is required" });
+            res.redirect('/profile');
+        }
+    }
+    else{
+        //return res.status(401).send({ "message": "A `full name` is required" });
+        res.redirect('/profile');
+    }
+
 });
 
+
 app.post('/add-quote', (req, res) => {
+
+
+    class PricingModule {
+
+    }
+    
     //check if quantity is a integer and positive
     if(!isNaN(req.body.quantity) && req.body.quantity > 0 && req.body.quantity.length > 0) {
         //check if date is string and is a date
@@ -81,12 +186,34 @@ app.post('/add-quote', (req, res) => {
 });
 
 
-/*
-app.post('/getlogin', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
+
+
+
+app.post('/get-login', (req,res)=>{
+    var Username = req.body.Username;
+    var Password = req.body.Password;
+
+    var match = Person_array.find((person, index)=>{
+        if(person.Username === Username && person.Password === Password){
+            return true;
+        }
+    });
+    if(match){
+        req.session.loggedin = true;
+        req.session.Username = Username;
+        res.redirect('/');
+        res.end();
+    }
+    else{
+        
+        res.redirect('login');
+        res.end();
+        
+    }
 });
-*/
+
+
+
 
 /*
     //404
--- test/app.test.js
+++ test/app.test.js
@@ -12,52 +12,30 @@ chai.use(chaiHttp);
 
 describe('app', ()=>{
 
+    describe('Statuses', function(){
+        it('Index should return 200 Status', function(done){
+            request(app)
+            .get('/').expect(200).end(done);
+        });
+        it('Quote should return 200 Status', function(done){
+            request(app)
+            .get('/quote').expect(200).end(done);
+        });
+        it('Login should return 200 Status', function(done){
+            request(app)
+            .get('/login').expect(200).end(done);
+        });
+        it('Register should return 200 Status', function(done){
+            request(app)
+            .get('/register').expect(200).end(done);
+        });
+        it('Quote History should return 200 Status', function(done){
+            request(app)
+            .get('/quote_history').expect(200).end(done);
+        });
+    });
+
     describe('Quote input and redirections', function(){
-        /*
-        it('Quote should return 200 Status', function(done){
-            request(app)
-            .get('/quote').expect(200).end(done);
-        });
-
-        //good input
-        it('Quote POST should accept a good input', function(done){
-            request(app)
-            .post('/add-quote').type('form').send({quantity: 23, address: "1234 Street", delivery: '2020-03-23'}).then(function(){
-                request(app).get('/quote_history').expect(200).expect(/Quote History/, done);
-            });
-        });
-
-        //bad quantity
-        it('Quote POST should not accept a bad quantity', function(done){
-            request(app)
-            .post('/add-quote').type('form').send({quantity: "bad", address: "1234 Street", delivery: '2020-03-23'}).then(function(){
-                request(app).get('/quote').expect(200).expect(/Get Quote/, done);
-            });
-        });
-        
-        //bad date
-        it('Quote POST should not accept a bad date', function(done){
-            request(app)
-            .post('/add-quote').type('form').send({quantity: 1234, address: "1232 Street", delivery: 'hello'}).then(function(){
-                request(app).get('/quote').expect(200).expect(/Get Quote/, done);
-            });
-        });
-
-        //bad address
-        it('Quote POST should not accept a bad address', function(done){
-            request(app)
-            .post('/add-quote').type('form').send({quantity: 23, address: 234, delivery: '2020-03-23'}).then(function(){
-                request(app).get('/quote').expect(200).expect(/Get Quote/, done);
-            });
-        });
-        */
-
-
-
-        it('Quote should return 200 Status', function(done){
-            request(app)
-            .get('/quote').expect(200).end(done);
-        });
 
         //good input
         it('Quote POST should accept a good input', function(done){
@@ -92,9 +70,104 @@ describe('app', ()=>{
             .post('/add-quote')
             .type('form')
             .send({quantity: 23, address: 234, delivery: '2020-03-23'})
-            .expect(/quotes/)
+            .expect(/quote/)
             .end(done);
         });
+
+    });
+  
+    //Profile test
+    describe('profile', ()=>{
+        it('profile should return OK status', function(done){
+            request(app)
+            .get('/profile').expect(200).end(done);
+            /*.then(function(response){
+                assert.equal(response.status, 200)
+            })*/
+        });
+
+        //good input
+        it('profile POST should accept a good input', function(done){
+            request(app)
+            .post('/add-profile')
+            .type('form')
+            .send({fullname: 'Ruth Soto', address1: '123 Smith St.', address2: '', city: 'Houston', state: 'TX', zipcode: 77123})
+            .expect(/\//)
+            .end(done);
+        });
+        //bad input for name
+        it('profile POST should not accecpt a bad name', function(done){
+            request(app)
+            .post('/add-profile')
+            .type('form')
+            .send({fullname: '&hfnie((', address1: '123 Smith St.', address2: '', city: 'Houston', state: 'TX', zipcode: 77123})
+            .expect(/profile/)
+            .end(done);
+            
+        });
+        //bad input for address1
+        it('profile POST should not accecpt a bad address1', function(done){
+            request(app)
+            .post('/add-profile').type('form').send({fullname: 'Ruth Soto', address1: 123, address2: '', city: 'Houston', state: 'TX', zipcode: 77123})
+            .expect(/profile/)
+            .end(done);
+        });
+        //bad input for address2
+        it('profile POST should not accecpt a bad address2', function(done){
+            request(app)
+            .post('/add-profile').type('form').send({fullname: 'Ruth Soto', address1: '123 Smith St.', address2: 583, city: 'Houston', state: 'TX', zipcode: 77123})
+            .expect(/profile/)
+            .end(done);
+        });
+        //bad input for city
+        it('profile POST should not accecpt a bad city', function(done){
+            request(app)
+            .post('/add-profile').type('form').send({fullname: 'Ruth Soto', address1: '123 Smith St.', address2: '', city: 'nvief8439(@', state: 'TX', zipcode: 77123})
+            .expect(/profile/)
+            .end(done);
+        });
+        //bad input for state
+        it('profile POST should not accecpt a empty state', function(done){
+            request(app)
+            .post('/add-profile').type('form').send({fullname: 'Ruth Soto', address1: '123 Smith St.', address2: '', city: 'Houston', state: '', zipcode: 77123})
+            .expect(/profile/)
+            .end(done); 
+        });
+        //bad input for zipcode
+        it('profile POST should not accecpt a bad zipcode', function(done){
+            request(app)
+            .post('/add-profile').type('form').send({fullname: 'Ruth Soto', address1: '123 Smith St.', address2: '', city: 'Houston', state: 'TX', zipcode: 77})
+            .expect(/profile/)
+            .end(done); 
+        });
+    });
+
+
+    //Mario's test section
+
+
+   describe('login', ()=>{
+    it('login should return 200 Status', function(done){
+        request(app)
+        .get('/login').expect(200).end(done);
+        });
+
+
+        it('login POST should accept a good input', function(done){
+            request(app)
+            .post('/get-login').type('form').send({Username: 'Mario123', Password: 'Houston16'}).then(function(){
+                request(app).get('/').expect(200).expect(/MSR Fuel/, done);
+            });
+        });
+     
+    it('login POST should not accept wrong input', function(done){
+        request(app).post('/get-login').type('form')
+        .send({Username: 'Wrong', Password: 'Input'}).expect(/login/).end(done);
+        
+           
+        });
+        
+
 
     });
 });
