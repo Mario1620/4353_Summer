d471abf305bb0cb19a1f0534584becea67b2e8c6
resoto1 <60246861+resoto1@users.noreply.github.com>
2020-07-12 11:54:47 -0500

    Add the insert for profile and change the order

--- test/app.test.js
+++ test/app.test.js
@@ -39,48 +39,89 @@ describe('app', ()=>{
         });
     });
 
-    describe('Quote input and redirections', function(){
+    //Mario's test section
+    describe('login', ()=>{
+    it('login should return 200 Status', function(done){
+        request(app)
+        .get('/login').expect(200).end(done);
+        });
 
-        //good input
-        it('Quote POST should accept a good input', function(done){
+
+        it('login POST should accept a good input and send to index if Status != New', function(done){
             request(app)
-            .post('/add-quote')
-            .type('form')
-            .send({quantity: 23, address: "1234 Street", delivery: '2020-03-23'})
-            .expect(/quote_history/)
-            .end(done);
+            .post('/get-login').type('form').send({Username: 'Mario123', Password: 'Houston16'}).then(function(){
+                request(app).get('/').expect(200).expect(/MSR Fuel/, done);
+            });
         });
-        //bad quantity
-        it('Quote POST should not accept a bad quantity (is string/empty/negative)', function(done){
-            request(app)
-            .post('/add-quote')
-            .type('form')
-            .send({quantity: "bad", address: "1234 Street", delivery: '2020-03-23'})
-            .expect(/quote/)
-            .end(done);
+     
+    it('login POST should not accept wrong input', function(done){
+        request(app).post('/get-login').type('form')
+        .send({Username: 'Wrong', Password: 'Input'}).expect(/login/).end(done);
+        
+           
         });
-        //bad date
-        it('Quote POST should not accept a bad date (not in date format)', function(done){
-            request(app)
-            .post('/add-quote')
-            .type('form')
-            .send({quantity: 1234, address: "1232 Street", delivery: 'hello'})
-            .expect(/quote/)
-            .end(done);
-        });
-        //bad address
-        it('Quote POST should not accept a bad address (is a number)', function(done){
-            request(app)
-            .post('/add-quote')
-            .type('form')
-            .send({quantity: 23, address: 234, delivery: '2020-03-23'})
-            .expect(/quote/)
-            .end(done);
-        });
+
+    it('login POST should accept good input and send to profile if Status == New', function(done){
+        request(app).post('/get-login').type('form').send({Username: 'Testing123', Password: 'Password01'})
+        
+            .expect(/profile/, done);
+        
+    });
 
     });
+
+    describe('Register', function(){
+        it('Register GET request should return 200', function(done){
+            request(app).get('/register').expect(200).end(done);
+        });
+        //This test needs to be updated every time with a new user that doesnt exists
+        it('Register POST should add a new user', function(done){
+            request(app).post('/add-user').type('form').send({Username: 'Testingx1', Password: 'Password01', ConfirmPassword: 'Password01'})
+            .then(function(){
+                request(app).get('/login').expect(/login/).end(done);
+            });
+            
+        });
+
+        //This should work but SQL prevents it from running
+        it('Register POST should not add an existing user', function(done){
+            request(app).post('/add-user').type('form')
+            .send({Username: 'administrator1', Password: 'Password01', ConfirmPassword: 'Password01'})
+            .expect(/register/).end(done);
+            
+        });  
+
+        it('Register POST should fail if Username does not meet requirements', function(done){
+            request(app).post('/add-user').type('form')
+            .send({Username: 'admin1', Password: 'Password01', ConfirmPassword: 'Password01'})
+            .expect(/register/).end(done);
+           
+
+        });
+        it('Register POST should fail if Password does not meet requirements', function(done){
+            request(app).post('/add-user').type('form')
+            .send({Username: 'administrator2', Password: 'Pass1', ConfirmPassword: 'Pass1'})
+            .expect(/register/).end(done);
+           
+
+        });
+
+        it('Register POST should fail if Passwords do not meet match', function(done){
+            request(app).post('/add-user').type('form')
+            .send({Username: 'adminstrator2', Password: 'Password01', ConfirmPassword: 'Password02'})
+            .expect(/register/).end(done);
+       
+        }); 
+    });
+   describe('logout', function(){
+        it('Logout GET request should return 200', function(done){
+
+            request(app).get('/logout').expect(302).end(done);
+        });
+    }); 
   
-    //Profile test
+  
+  //Profile test
     describe('profile', ()=>{
        //good input
         it('profile POST should accept a good input', function(done){
@@ -145,96 +186,57 @@ describe('app', ()=>{
             .expect(/profile/)
             .end(done); 
         });
-    });
-
-
-    //Mario's test section
-
-
-   describe('login', ()=>{
-    it('login should return 200 Status', function(done){
-        request(app)
-        .get('/login').expect(200).end(done);
-        });
-
-
-        it('login POST should accept a good input and send to index if Status != New', function(done){
+        //created one for the insert from profile
+        it('profile POST should good Profile INSERT', function(done){
             request(app)
-            .post('/get-login').type('form').send({Username: 'Mario123', Password: 'Houston16'}).then(function(){
-                request(app).get('/').expect(200).expect(/MSR Fuel/, done);
-            });
+            .post('/add-profile')
+            .type('form')
+            .send({Username: 'Testingx1', fullname: 'John Doe', address1: '123 Main St.', address2: '', city: 'NYC', states: 'NY', zipcode: 77555})
+            .expect(/\//)
+            .end(done);
         });
-     
-    it('login POST should not accept wrong input', function(done){
-        request(app).post('/get-login').type('form')
-        .send({Username: 'Wrong', Password: 'Input'}).expect(/login/).end(done);
-        
-           
-        });
-
-    it('login POST should accept good input and send to profile if Status == New', function(done){
-        request(app).post('/get-login').type('form').send({Username: 'Testing123', Password: 'Password01'})
-        
-            .expect(/profile/, done);
-        
     });
-        
+  
+  //Quotes
+    describe('Quote input and redirections', function(){
 
-
-    });
-
-    describe('Register', function(){
-        it('Register GET request should return 200', function(done){
-            request(app).get('/register').expect(200).end(done);
+        //good input
+        it('Quote POST should accept a good input', function(done){
+            request(app)
+            .post('/add-quote')
+            .type('form')
+            .send({quantity: 23, address: "1234 Street", delivery: '2020-03-23'})
+            .expect(/quote_history/)
+            .end(done);
         });
-        //This test needs to be updated every time with a new user that doesnt exists
-        it('Register POST should add a new user', function(done){
-            request(app).post('/add-user').type('form').send({Username: 'Testingx1', Password: 'Password01', ConfirmPassword: 'Password01'})
-            .then(function(){
-                request(app).get('/login').expect(/login/).end(done);
-            });
-            
+        //bad quantity
+        it('Quote POST should not accept a bad quantity (is string/empty/negative)', function(done){
+            request(app)
+            .post('/add-quote')
+            .type('form')
+            .send({quantity: "bad", address: "1234 Street", delivery: '2020-03-23'})
+            .expect(/quote/)
+            .end(done);
         });
-
-        //This should work but SQL prevents it from running
-        it('Register POST should not add an existing user', function(done){
-            request(app).post('/add-user').type('form')
-            .send({Username: 'administrator1', Password: 'Password01', ConfirmPassword: 'Password01'})
-            .expect(/register/).end(done);
-            
-        });  
-
-        it('Register POST should fail if Username does not meet requirements', function(done){
-            request(app).post('/add-user').type('form')
-            .send({Username: 'admin1', Password: 'Password01', ConfirmPassword: 'Password01'})
-            .expect(/register/).end(done);
-           
-
+        //bad date
+        it('Quote POST should not accept a bad date (not in date format)', function(done){
+            request(app)
+            .post('/add-quote')
+            .type('form')
+            .send({quantity: 1234, address: "1232 Street", delivery: 'hello'})
+            .expect(/quote/)
+            .end(done);
         });
-        it('Register POST should fail if Password does not meet requirements', function(done){
-            request(app).post('/add-user').type('form')
-            .send({Username: 'administrator2', Password: 'Pass1', ConfirmPassword: 'Pass1'})
-            .expect(/register/).end(done);
-           
-
+        //bad address
+        it('Quote POST should not accept a bad address (is a number)', function(done){
+            request(app)
+            .post('/add-quote')
+            .type('form')
+            .send({quantity: 23, address: 234, delivery: '2020-03-23'})
+            .expect(/quote/)
+            .end(done);
         });
-
-        it('Register POST should fail if Passwords do not meet match', function(done){
-            request(app).post('/add-user').type('form')
-            .send({Username: 'adminstrator2', Password: 'Password01', ConfirmPassword: 'Password02'})
-            .expect(/register/).end(done);
-       
-        }); 
-
-
-
 
     });
-  describe('logout', function(){
-        it('Logout GET request should return 200', function(done){
-
-            request(app).get('/logout').expect(302).end(done);
-        });
-    }); 
 });
   
