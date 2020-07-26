990c73d636a81a15c22643cd3c355bae3704425f
resoto1 <60246861+resoto1@users.noreply.github.com>
2020-07-26 16:24:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -200,7 +200,8 @@ app.post('/add-profile', (req,res) => {
                             DeliveryDate: req.body.delivery,
                             Address: results[0].Address1,
                             Username:  req.body.quoteUser,
-                            Total: req.body.quantity*5.00     
+                            //Total: req.body.quantity*5.00 
+                    	    Total: formula(req.body.quantity, results[0].State, results)     
                         };
                         
                         sql = 'INSERT INTO QuoteHistory SET ?';
@@ -222,6 +223,72 @@ app.post('/add-profile', (req,res) => {
             res.redirect('/quote');
         }
     });
+
+function formula(Gallons, State, result) {//formula for get quote page
+    /*
+    Suggested Price = Current Price + Margin
+    Where,
+    Current price per gallon = $1.50 (this is the price what distributor gets from refinery and it varies based upon crude price. But we are keeping it constant for simplicity)
+    Margin =  Current Price * (Location Factor - Rate History Factor + Gallons Requested Factor + Company Profit Factor)
+
+    Consider these factors:
+    Location Factor = 2% for Texas, 4% for out of state.
+    Rate History Factor = 1% if client requested fuel before, 0% if no history (you can query fuel quote table to check if there are any rows for the client)
+    Gallons Requested Factor = 2% if more than 1000 Gallons, 3% if less
+    Company Profit Factor = 10% always
+    */
+
+    var suggestPrice;
+    var currentPrice = 1.50;
+    var margin;
+    var locationFactor;
+    //var state; //
+    var rateHistory;
+    //var rateFuel; //quoteHistory
+    var gallonsRequested;
+    //var gallons
+    var companyProfit = 10/100;
+    //var totalAmount;
+
+
+    //gallons requested factor
+    if (Gallons > 1000) {
+        gallonsRequested = 2/100;
+    }
+    else {
+        gallonsRequested = 3/100;
+    }
+    console.log(gallonsRequested);  
+
+    //location factor
+    if (State == "TX") {
+        
+        locationFactor = 2/100;
+    }
+    else {
+        locationFactor = 4/100;
+    }
+    console.log(locationFactor);    
+
+    console.log(result);
+    if(result.length === 0) {
+        rateHistory = 0;
+    }
+    else{
+        rateHistory = 1/100;
+    }
+    console.log(rateHistory); 
+
+    margin = currentPrice * (locationFactor - rateHistory + gallonsRequested + companyProfit);
+    //console.log(margin);
+    suggestPrice = currentPrice + margin;
+    //console.log(suggestPrice);
+
+    totalAmount = Gallons * suggestPrice;
+    console.log(totalAmount);    
+    
+    return totalAmount;
+};
     
 
 //login post
